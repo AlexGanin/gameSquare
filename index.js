ec13451033c908ba5356113d1939baf4fb9acb0c
AlexGanin <711seller@gmail.com>
2021-04-28 20:05:14 +0300

    2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let counter = 0
 const $timeHeader = document.querySelector('#time-header')
 const $resultHeader = document.querySelector('#result-header')
 const $start = document.querySelector('#start')
@@ -6,8 +5,11 @@ const $game = document.querySelector('#game')
 const $gameTime = document.querySelector('#game-time')
 const $time = document.querySelector('#time')
 const $result = document.querySelector('#result')
+
+let counter = 0
 const dataGame = $game.getBoundingClientRect()
 const background = ['red', 'blue', 'green', 'yellow', 'gold']
+
 
 function hide(el) {
   el.classList.add('hide')
@@ -24,11 +26,11 @@ function randomNum(min, max) {
 function seconds() {
   let timer = setInterval(function() {
     if($time.textContent > 0) {
-      $time.textContent = ($time.textContent - 0.100).toFixed(1)
+      $time.textContent = ($time.textContent - 0.1).toFixed(1)
       return
     } 
-  clearInterval(timer)
-  finish()
+    clearInterval(timer)
+    finish()
   }, 100)
 }
 
@@ -37,11 +39,15 @@ function start() {
   hide($resultHeader)
   hide($start)
   show($timeHeader)
-  $time.innerHTML = $gameTime.value
+  setGameTime()
   $gameTime.setAttribute('disabled', true)
   $game.style.backgroundColor = '#fff'
   renderShare()
   seconds()
+}
+
+function setGameCounter() {
+  $result.textContent = counter
 }
 
 function finish() {
@@ -49,44 +55,43 @@ function finish() {
   $game.removeAttribute('style')
   $game.innerHTML = ''
   hide($timeHeader)
-  $result.textContent = counter
+  setGameCounter()
   show($resultHeader)
   show($start)
 }
 
 function renderShare() {
+  let randomColor = randomNum(0, background.length)
+  $game.innerHTML = ''
   let squareSize = randomNum(10,100)
   let $square = document.createElement('div')
-  $square.style.backgroundColor = background[randomNum(0, background.length)]
+  const maxTop = dataGame.height - squareSize
+  const maxLeft = dataGame.width - squareSize
+  $square.style.backgroundColor = background[randomColor]
   $square.style.width = $square.style.height = squareSize + 'px'
   $square.style.cursor = 'pointer'
   $square.style.position = 'absolute'
-  $square.style.top = randomNum(0, dataGame.height - squareSize) + 'px'
-  $square.style.left = randomNum(0, dataGame.width - squareSize) + 'px'
+  $square.style.top = randomNum(0, maxTop) + 'px'
+  $square.style.left = randomNum(0, maxLeft) + 'px'
   $square.setAttribute('data-square', true)
   $game.insertAdjacentHTML('afterbegin', $square.outerHTML)
 }
 
-function gameCounter() {
-  $game.addEventListener('click', function(event) {
-    if(event.target.dataset.square) {
-      $game.innerHTML = ''
-      renderShare()
-      counter++
-    }
-  })
+function shapeClicker(event) {
+  if(event.target.dataset.square) {
+    renderShare()
+    counter++
+  }
 }
 
-gameCounter()
-
-$start.addEventListener('click', function() {
-  start()
-})
-
-$gameTime.addEventListener('input', function(event) {
-
+function setGameTime() {
   hide($resultHeader)
   show($timeHeader)
-  $time.textContent = this.value + '.0'
+  $time.innerHTML = $gameTime.value
+}
 
-})
+$game.addEventListener('click', shapeClicker)
+
+$start.addEventListener('click', start)
+
+$gameTime.addEventListener('input', setGameTime)
